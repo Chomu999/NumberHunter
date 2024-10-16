481920cd51db04c67bc0ed82287478c375e4546b
Chomu999 <84760026+Chomu999@users.noreply.github.com>
2024-10-16 16:03:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -26,10 +26,12 @@ return outColor;
 
 
 static RandomPhoneNumber(){
-let out="";
+let out=[];
 for(let i=0;i<10;i++){
-out += Rand.Int(0, 9);
+out.push(Rand.Int(0, 9) );
 }
+out[0]=(out[0]===0)?Rand.Int(0, 9):out[0];
+out =out.join().split(",").join("");
 return out;
 }
 
@@ -46,24 +48,27 @@ const INITIAL=async()=>{
 const numberContainer = document.querySelector(".numberContainer");
 
 const genNumBtn=document.querySelector('.genNumBtn');
+const genMaxNum=document.querySelector('.genMaxNum');
 
 
 
 const getRandomNumberElementChild=()=>{
 
 const randomNumber = Rand.RandomPhoneNumber();
+const Element= document.createElement("span");
 
-const numEln= document.createElement("span");
-numEln.innerHTML = `
-	
-	<i class="addNumberBtn fa fa-2x fa-whatsapp">add</i>
+
+Element.innerHTML = `
+	<i class="add_whatsapp_Btn fa fa-3x fa-whatsapp"></i>
 	
 	<span class="number wa">${randomNumber}</span>
+	
+	<i class="add_telegram_Btn fa fa-3x fa-telegram"></i>
 `;
 
-numEln.classList.add("huntNumber");
+Element.classList.add("huntNumber");
 
-return numEln;
+return Element;
 }
 
 
@@ -71,29 +76,31 @@ return numEln;
 // generate random phone number and appending at parent container
 const generateNumber=()=>{
 numberContainer.innerHTML="";
-let maxNumber=5;
+let maxNumber=parseInt(genMaxNum.innerText)||1;
 for(let i=0;i<maxNumber;i++){
 const chlid = getRandomNumberElementChild();
 numberContainer.appendChild(chlid);
 }
+
 }
 
 //calling first time for onces
-generateNumber()
+generateNumber();
 
 
 
 
 //saving number from clipboard
 const saveNumber = (number)=>{
-let save = `wa.me/91${number}`;
+let save = `+91${number}`;
 navigator.clipboard.writeText(save);
 }
 
 
 // number redirect to browser to wa
-const redirectNumber = (number)=>{
-let redirect = `https://wa.me/91${number}`;
+const redirectNumber = (number, type=true)=>{
+let platform = (type===true)?"wa.me":"t.me";
+let redirect = `https://${platform}/91${number}`;
 window.location.assign(redirect);
 }
 
@@ -108,26 +115,26 @@ const element = e?.srcElement;
 
 const condition = element.classList[0];
 
-if(condition === "huntNumber"){
-const textNumber = element.lastElementChild.innerText;
-saveNumber(textNumber);
-redirectNumber(textNumber);
-}
-else if(condition === "addNumberBtn"){
+if(condition === "huntNumber") return false;
+
+else if(condition === "add_whatsapp_Btn"){
 const textNumber = element.nextElementSibling.innerText;
 saveNumber(textNumber);
-redirectNumber(textNumber);
+redirectNumber(textNumber, true);
 }
+
 else if (condition === "number"){
 const textNumber = element.innerText;
 saveNumber(textNumber);
-redirectNumber(textNumber);
-}
-else{
-console.log("something wrong");
 }
 
+else if(condition === "add_telegram_Btn"){
+const textNumber = element.previousElementSibling.innerText;
+saveNumber(textNumber);
+redirectNumber(textNumber, false);
+}
 
+else return false;
 
 
 }
